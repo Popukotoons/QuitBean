bee1f89e4f5c9b936b0253e5340be75125bdf399
Popuko <olanc06@gmail.com>
2020-08-26 07:57:55 +0100

    Add files via upload

---
+++ BeanQuit.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class beanquit : MonoBehaviour
+{
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        Application.Quit();
+    }
+}
